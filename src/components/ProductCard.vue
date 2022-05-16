f2a801837712f808948c0af4d5025068c3097873
naiadidub <antonio.zaghi1405@gmail.com>
2022-05-16 23:23:43 +0200

    dio

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h6>{{titolo}}</h6>
+        <div>
+            <i v-for="(n, index) in votoInt" :key="index" class="fa-solid fa-star"></i>
+        </div>
+        <h5>${{prezzo}}</h5>   
+        <img :src="require(`${src}`)" alt="">     
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductCard',
+    props:{
+        titolo: String,
+        prezzo: Number,
+        src: String,
+        vote: Number,
+        prezzoScontato: String
+    },
+    computed: {
+        votoInt(){
+            return this.vote
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
