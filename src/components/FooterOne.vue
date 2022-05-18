a531b9a4e50b54e71b3567bfda36453646916972
naiadidub <antonio.zaghi1405@gmail.com>
2022-05-18 11:34:03 +0200

    aggiunta header

--- src/components/FooterOne.vue
+++ src/components/FooterOne.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container my-5">
-        <div class="row">
+        <div class="row justify-content-between">
             <FeaturedProducts/>
             <OnSaleProducts/>
             <TopRatedProducts/>
